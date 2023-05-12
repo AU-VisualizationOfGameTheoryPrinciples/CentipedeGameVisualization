c9f3ce26f84920de184eaeab8b5da1d7cc409754
M-Gentes <manuel.gentes@gmail.com>
2023-05-12 23:24:21 +0200

    change game ending alert

--- setScore.js
+++ setScore.js
@@ -4,6 +4,7 @@ var player_turn = document.querySelector("#player_turn");
 var round_value = document.querySelector("#round_value");
 
 const round_add = 10;
+const ENDING_ROUND = 100;
 
 const CENTIPEDE_MOVE = { CONTINUE: 0, END: 1, HONOR: 2, DEFECT: 3, length: 4 };
 var arrayCentipedeName = [3];
@@ -22,10 +23,38 @@ function setPlayerTurn(player_num){
 function getPlayerTurn(){
     return Number.parseInt(player_turn.innerText);
 }
-function endGame(){
+function endGame(ending_move){
     buttonContinue.disabled = "true";
     buttonEnd.disabled = "true";
-    alert("P1: " + getScore(1) + " P2: " + getScore(2));
+
+    let currentPlayer = getPlayerTurn();
+    let otherPlayer = (getPlayerTurn()+2)%2+1;
+
+    let finishingMessage = "P1: " + getScore(1) + " P2: " + getScore(2) + "\n";
+    finishingMessage+= "Player " + currentPlayer + " has ended the Game ";
+    let winningMessage = "He has " + (getScore(currentPlayer) - getScore(otherPlayer)) + " more bucks than Player " + otherPlayer + "!";
+    switch(ending_move){
+        case CENTIPEDE_MOVE.END: {
+            if(checkRound(0)){
+                finishingMessage+= "right away!\n";
+            } else {
+                finishingMessage+= "inbetween.\n";
+            }
+            finishingMessage+= winningMessage;
+            break;
+        }
+        case CENTIPEDE_MOVE.DEFECT: {
+            finishingMessage+= "and defected at the last turn.\n";
+            finishingMessage+= winningMessage;
+            break;
+        }
+        case CENTIPEDE_MOVE.HONOR: {
+            finishingMessage+= "and shared the money with Player " + otherPlayer + ".\n";
+            finishingMessage+= "They both won " + getScore(1) + "$!";
+            break;
+        }
+    }
+    alert(finishingMessage);
 }
 
 setPlayerTurn(1);
@@ -36,19 +65,24 @@ function startGameRound(move){
     if(move == CENTIPEDE_MOVE.CONTINUE){
         setScore(1,1);
         setScore(2,1);
+        if(checkRound(ENDING_ROUND)){
+            endGame(CENTIPEDE_MOVE.HONOR);
+            return;
+        }
     } else if(move == CENTIPEDE_MOVE.END){
         setScore(getPlayerTurn(), 2);
-        endGame();
+        if(checkRound(ENDING_ROUND)){
+            endGame(CENTIPEDE_MOVE.DEFECT);
+        } else {
+            endGame(CENTIPEDE_MOVE.END);
+        }
         return;
     }
 
     addRound();
-    if(checkRound(100)){
+    if(checkRound(ENDING_ROUND)){
         buttonContinue.textContent = arrayCentipedeName[CENTIPEDE_MOVE.HONOR];
         buttonEnd.textContent = arrayCentipedeName[CENTIPEDE_MOVE.DEFECT];
-    } else if(checkRound(100+round_add)){
-        endGame();
-        return;
     }
     setPlayerTurn((getPlayerTurn()+2)%2+1);
 }
